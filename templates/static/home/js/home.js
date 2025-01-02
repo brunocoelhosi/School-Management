6d2bca2824c25f1c13f5b9636792cd2547ad761a
brunocoelhosi <brunocoelho.si@outlook.com>
2025-01-02 17:27:33 -0300

    update front

--- templates/static/home/js/home.js
+++ templates/static/home/js/home.js
@@ -1,4 +1,48 @@
+////Fechar navbar ao clicar fora dela
+document.addEventListener('click', function(event) {
+    var navbarCollapse = document.getElementById('navbarNav');
+    var isClickInsideNavbar = navbarCollapse.contains(event.target);
+    var isNavbarToggler = document.querySelector('.navbar-toggler').contains(event.target);
 
+    if (!isClickInsideNavbar && !isNavbarToggler && navbarCollapse.classList.contains('show')) {
+        var bsCollapse = new bootstrap.Collapse(navbarCollapse);
+        bsCollapse.hide();
+    }
+});
+
+/////// Fechar a navbar ao selecionar uma das opções de menu
+document.querySelectorAll('.navbar-nav .nav-link, .scrollToCard').forEach(function(navLink) {
+    navLink.addEventListener('click', function() {
+        var navbarCollapse = document.getElementById('navbarNav');
+        if (navbarCollapse.classList.contains('show')) {
+            var bsCollapse = new bootstrap.Collapse(navbarCollapse);
+            bsCollapse.hide();
+        }
+    });
+});
+
+
+/////Evento para centralizar ao selecionar opcao
+// Seleciona todos os links com a classe "scrollToCard"
+const buttons = document.querySelectorAll('.scrollToCard');
+
+// Adiciona o evento de clique para cada botão
+buttons.forEach(button => {
+  button.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevenir o comportamento padrão do link
+    
+    // Seleciona o card de matrícula
+    const card = document.getElementById('cardmat');
+    
+    // Faz o scroll até o card centralizando-o
+    card.scrollIntoView({
+      behavior: 'smooth', // Suaviza o movimento de rolagem
+      block: 'center'     // Centraliza o card verticalmente
+    });
+  });
+});
+
+////////evento da section cursos
 document.addEventListener('DOMContentLoaded',() => {
     new TypeIt('.animated',{
 
